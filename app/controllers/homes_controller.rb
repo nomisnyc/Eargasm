8eac69e5a735366c30234c60c8515dcefd0a7f8e
Simon Wong <nomisnyc85@gmail.com>
2013-02-17 23:31:46 -0500

    3rd commit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,56 +2,36 @@ class HomesController < ApplicationController
 
   def index
     @homes = Home.all
-    end
   end
 
-  # GET /homes/1
-  # GET /homes/1.json
   def show
     @home = Home.find(params[:id])
   end
 
-  # GET /homes/new
-  # GET /homes/new.json
   def new
     @home = Home.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @home }
-    end
   end
 
-  # GET /homes/1/edit
   def edit
     @home = Home.find(params[:id])
   end
 
-  # POST /homes
-  # POST /homes.json
+
   def create
     @home = Home.new(params[:home])
-
-
-    end
+    redirect_to(homes_path)
   end
 
   def update
-    @home = Home.find(params[:id])
-
-    respond_to do |format|
-      if @home.update_attributes(params[:home])
-        format.html { redirect_to @home, notice: 'Home was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @home.errors, status: :unprocessable_entity }
-      end
+    home = Home.find(params[:id])
+    home.update_attributes(params[:home])
+    redirect_to(home_path)
+  end
 
   def destroy
     @home = Home.find(params[:id])
     @home.destroy
+    redirect_to(home_path)
 
-    end
   end
 end
