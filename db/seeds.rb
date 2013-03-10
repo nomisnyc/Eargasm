a5ca571dd7bd805924ce471634f762ea185caad0
Adi Singh <adityasingh89@gmail.com>
2013-03-10 17:56:02 -0400

    youtube query

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 Music.delete_all
 
 
-Music.create(:name =>'Jacks Mannequin ', :song_name => 'Swim', :rating => 5, :genre => ' alternative', :url => ' http://www.youtube.com/watch?v=sA8PaIw5gcE ', :post_by => ' Simon')
+Music.create(:name =>'Jacks Mannequin ', :song_name => 'Swim', :rating => 5, :genre => ' alternative', :url => ' https://www.youtube.com/embed/sA8PaIw5gcE ', :post_by => ' Simon')
 
-Music.create(:name =>' Coldplay', :song_name => ' Paradise ', :rating => '6 ', :genre => ' alternative ', :url => 'http://www.youtube.com/watch?v=1G4isv_Fylg ', :post_by => 'Simon ')
+Music.create(:name =>' Coldplay', :song_name => ' Paradise ', :rating => '6 ', :genre => ' alternative ', :url => 'https://www.youtube.com/embed/1G4isv_Fylg ', :post_by => 'Simon ')
 
 
-Music.create(:name =>' Modest Mouse', :song_name => ' Float On', :rating => '7 ', :genre => 'indie', :url => 'http://www.youtube.com/watch?v=CTAud5O7Qqk ', :post_by => ' Simon')
+Music.create(:name =>' Modest Mouse', :song_name => ' Float On', :rating => '7 ', :genre => 'indie', :url => 'https://www.youtube.com/embed/CTAud5O7Qqk ', :post_by => ' Simon')
 
