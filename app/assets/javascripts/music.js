03c6c8701b8942424f1a50dffa29b0873c8ea95d
Simon Wong <nomisnyc85@gmail.com>
2013-03-11 14:55:55 -0400

    fixed stuff

--- app/assets/javascripts/music.js
+++ app/assets/javascripts/music.js
@@ -1,10 +1,20 @@
  $(function(){
   $('#artist_bio').click(toggle_bio);
+  $('#artist_location').click(toggle_location);
+  $('artist_event').click(toggle_event);
  });
 
 function toggle_bio()
 {
   $('.artistbio').toggle();
-
 }
 
+function toggle_location()
+{
+  $('.artist_location').toggle();
+}
+
+function toggle_event()
+{
+  $('.artist_event').toggle();
+}
