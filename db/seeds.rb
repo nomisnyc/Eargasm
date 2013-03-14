fadb5f33d83cdf52bf0f159db2e3ab30497dbea0
Simon Wong <nomisnyc85@gmail.com>
2013-03-13 20:48:51 -0400

    pulling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Music.delete_all
 User.delete_all
 
-m1 = Music.create(:name =>'Chris Brown', :song_name => 'Dont Wake Me Up', :rating => 5, :genre => 'R&B', :url => ' hhttp://www.youtube.com/watch?v=QOowQeKyNkQ', :image_url => 'http://i2.cdn.turner.com/cnn/dam/assets/120911071015-chris-brown-mtv-vma-2012-award-story-top.jpg')
+m1 = Music.create(:name =>'Chris Brown', :song_name => 'Dont Wake Me Up', :rating => 5, :genre => 'R&B', :url => 'http://www.youtube.com/watch?v=QOowQeKyNkQ', :image_url => 'http://i2.cdn.turner.com/cnn/dam/assets/120911071015-chris-brown-mtv-vma-2012-award-story-top.jpg')
 
 m2= Music.create(:name =>'Coldplay', :song_name => ' Paradise ', :rating => '6 ', :genre => ' alternative ', :url => 'https://www.youtube.com/embed/1G4isv_Fylg ', :post_by => 'Simon', :image_url => 'http://www.jonathanfun.com/wp-content/uploads/2010/11/coldplay-1.jpg')
 
