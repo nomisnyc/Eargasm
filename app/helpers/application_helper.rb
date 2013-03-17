7b4fe23d937bf8262d848266a5dd4a132256af34
Adi Singh <adityasingh89@gmail.com>
2013-03-16 20:17:17 -0400

    formatted idx page and made new music page ajax style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def intellinav
     if @auth.present?
       link_to(@auth.name, login_path, :method => :delete)
+
     else
       link_to('login', login_path, :id => 'logbutton', :remote => true)
     end
