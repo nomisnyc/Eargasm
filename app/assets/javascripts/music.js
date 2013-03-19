d8b33c00f31d7c73fe6006c3be42cd3378678e33
Simon Wong <nomisnyc85@gmail.com>
2013-03-18 20:08:28 -0400

    fix home page

--- app/assets/javascripts/music.js
+++ app/assets/javascripts/music.js
@@ -4,9 +4,9 @@ $(function(){
 $('h2.subheader').click(demo);
 
 $('#musicloop').on('click', '.upvote', up_vote);
-$('#artist_bio').click(toggle_bio);
+$('#artist_bio').click(hide_event);
 $('#artist_location').click(toggle_location);
-$('#artist_event').click(toggle_event);
+$('#artist_event').click(show_event);
 $('#homepage').click(toggle_home);
 $('.homepage').children().children().css('font-size','50px');
   $(".various").fancybox({
@@ -42,7 +42,7 @@ $('.homepage').children().children().css('font-size','50px');
 function toggle_bio()
 {
   $('.artistbio').toggle();
-  $('.artistevent').hide();
+  $('#artistevent').hide();
 }
 
 function toggle_location()
@@ -52,10 +52,44 @@ function toggle_location()
 
 function toggle_event()
 {
-  $('.artistevent').toggle();
+
+  if( $('#artistevent').is(":hidden") ){
+    show_event();
+  }else {
+    hide_event();
+  }
+
+}
+
+function show_event(){
+$('#artistevent').show();
   $('.artistbio').hide();
-  $('#map_canvas').toggle();
-  $('#distance').toggle();
+  $('#map_canvas').show();
+  $('#distance').show();
+
+
+
+      var map_center_lat = $('#map_center_lat').val();
+        var map_center_lng = $('#map_center_lng').val();
+        var music_venue_lat = $('#music_venue_lat').val();
+        var music_venue_lng = $('#music_venue_lng').val();
+        var music_venue_name = $('#music_venue_name').val();
+        var music_location = $('#music_location').val();
+        var music_distance = $('#music_distance').val();
+
+
+   display_map(map_center_lat,  map_center_lng , 11 );
+   add_marker(music_venue_lat, music_venue_lng, music_venue_name);
+
+
+}
+
+function hide_event(){
+$('#artistevent').hide();
+  $('.artistbio').show();
+  $('#map_canvas').hide();
+  $('#distance').hide();
+
 }
 
 function toggle_home()
